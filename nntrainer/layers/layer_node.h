268886d7dfa062ff84f8acef689d5442d159a285
SeungBaek Hong <sb92.hong@samsung.com>
2025-12-16 01:47:04 +0900

    [layers] update weight layer
     The current version of NNTrainer determines whether a layer is an input layer based on the p
    resence of an input shape. If there is no input shape, the layer is treated as an input layer, w
    hich causes a problem because weight layers also lack an input shape and are therefore mistakenl
    y considered input layers.
    
     To address this, the commit introduces a weight_dim property that sets the dimension of weig
    ht layers, allowing the system to distinguish between input layers and weight layers.
    
     However, this version is not the final set of changes. A separate PR incorporating the final
     modifications will be created in this week, and I will request a merge of that PR together with this one. The current version is just a working version commit before the SRIB commit.
    
    ** Things to be done:
     - Since a new property was added to layer_node, the current layer_impl cannot access it. I plan to improve this by parsing the property into a local variable during the finalize stage of layer_node so that it can be used at the layer_impl level.
     - Add unit tests for weight layer.
    
    Signed-off-by: SeungBaek Hong <sb92.hong@samsung.com>

--- nntrainer/layers/layer_node.h
+++ nntrainer/layers/layer_node.h
@@ -48,6 +48,7 @@ class Distribute;
 class Flatten;
 class Loss;
 class InputShape;
+class WeightDimension;
 class Activation;
 class SharedFrom;
 class InputConnection;
@@ -519,6 +520,12 @@ public:
    * @return bool true if input shape property has set
    */
   bool hasInputShapeProperty() const;
+  /**
+   * @brief check whether the layer is weight layer
+   *
+   * @return bool true if weight layer, else false
+   */
+  bool isWeightNode() const;
 
   /**
    * @brief Get the input dimension
@@ -1046,9 +1053,10 @@ properties in the context/graph unless intended. */
   using PropsType =
     std::tuple<props::Name, props::Distribute, props::Trainable,
                std::vector<props::InputConnection>,
-               std::vector<props::InputShape>, props::SharedFrom,
-               props::ClipGradByGlobalNorm, props::Packed, props::WeightDtype,
-               props::LossScaleForMixed, props::ComputeEngine>;
+               std::vector<props::InputShape>, props::WeightDimension,
+               props::SharedFrom, props::ClipGradByGlobalNorm, props::Packed,
+               props::WeightDtype, props::LossScaleForMixed,
+               props::ComputeEngine>;
 
   using RealizationPropsType = std::tuple<props::Flatten, props::Activation>;
   /** these realization properties results in addition of new layers, hence
