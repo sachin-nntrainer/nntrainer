3b347cfe5aaed6a1d1322e02bbc02700798d22de
SeungBaek Hong <sb92.hong@samsung.com>
2026-01-09 19:12:15 +0900

    [trivial] formatting for doxygen tag
    I applied doxygen tag formatting for tokenizers_cpp.h
    
    **Self evaluation:**
    1. Build test: [X]Passed [ ]Failed [ ]Skipped
    2. Run test: [X]Passed [ ]Failed [ ]Skipped
    
    Signed-off-by: SeungBaek Hong <sb92.hong@samsung.com>

--- Applications/CausalLM/tokenizers_cpp.h
+++ Applications/CausalLM/tokenizers_cpp.h
@@ -1,7 +1,9 @@
-/*!
+/**
  *  Copyright (c) 2023 by Contributors
- * \file tokenizers_cpp.h
- * \brief A C++ binding to common set of tokenizers
+ * @file tokenizers_cpp.h
+ * @brief A C++ binding to common set of tokenizers
+ * @author Contributors
+ * @bug No known bugs
  */
 #ifndef TOKENIZERS_CPP_H_
 #define TOKENIZERS_CPP_H_
@@ -12,36 +14,36 @@
 
 namespace tokenizers {
 
-/*!
- * \brief a universal tokenizer that loads
+/**
+ * @brief a universal tokenizer that loads
  *  either HF's tokenizer or sentence piece,
  *  depending on the constructor
  */
 class Tokenizer {
 public:
-  /*! \brief virtual destructor */
+  /** @brief virtual destructor */
   virtual ~Tokenizer() {}
 
-  /*!
-   * \brief Encode text into ids.
-   * \param text The input text.
-   * \returns The encoded token ids.
+  /**
+   * @brief Encode text into ids.
+   * @param text The input text.
+   * @return The encoded token ids.
    */
   virtual std::vector<int32_t> Encode(const std::string &text) = 0;
 
-  /*!
-   * \brief Encode text into ids with special tokens option.
-   * \param text The input text.
-   * \param add_special_tokens Whether to add special tokens.
-   * \returns The encoded token ids.
+  /**
+   * @brief Encode text into ids with special tokens option.
+   * @param text The input text.
+   * @param add_special_tokens Whether to add special tokens.
+   * @return The encoded token ids.
    */
   virtual std::vector<int32_t> Encode(const std::string &text,
                                       bool add_special_tokens) = 0;
 
-  /*!
-   * \brief Encode a batch of texts into ids.
-   * \param texts The input texts.
-   * \returns The encoded token ids.
+  /**
+   * @brief Encode a batch of texts into ids.
+   * @param texts The input texts.
+   * @return The encoded token ids.
    */
   virtual std::vector<std::vector<int32_t>>
   EncodeBatch(const std::vector<std::string> &texts) {
@@ -54,26 +56,26 @@ public:
     return ret;
   }
 
-  /*!
-   * \brief Decode token ids into text.
-   * \param text The token ids.
-   * \returns The decoded text.
+  /**
+   * @brief Decode token ids into text.
+   * @param text The token ids.
+   * @return The decoded text.
    */
   virtual std::string Decode(const std::vector<int32_t> &ids) = 0;
 
-  /*!
-   * \brief Returns the vocabulary size. Special tokens are considered.
+  /**
+   * @brief Returns the vocabulary size. Special tokens are considered.
    */
   virtual size_t GetVocabSize() = 0;
 
-  /*!
-   * \brief Convert the given id to its corresponding token if it exists. If
+  /**
+   * @brief Convert the given id to its corresponding token if it exists. If
    * not, return an empty string.
    */
   virtual std::string IdToToken(int32_t token_id) = 0;
 
-  /*!
-   * \brief Convert the given token to its corresponding id if it exists. If
+  /**
+   * @brief Convert the given token to its corresponding id if it exists. If
    * not, return -1.
    */
   virtual int32_t TokenToId(const std::string &token) = 0;
@@ -83,38 +85,38 @@ public:
   // These factory function takes in in-memory blobs
   // so the library can be independent from filesystem
   //---------------------------------------------------
-  /*!
-   * \brief Create HF tokenizer from a single in-memory json blob.
+  /**
+   * @brief Create HF tokenizer from a single in-memory json blob.
    *
-   * \param json_blob The json blob.
-   * \return The created tokenzier.
+   * @param json_blob The json blob.
+   * @return The created tokenzier.
    */
   static std::unique_ptr<Tokenizer> FromBlobJSON(const std::string &json_blob);
-  /*!
-   * \brief Create BPE tokenizer
+  /**
+   * @brief Create BPE tokenizer
    *
-   * \param vocab_blob The blob that contains vocabs.
-   * \param merges_blob The blob that contains the merges.
-   * \param added_tokens The added tokens.
-   * \return The created tokenizer.
+   * @param vocab_blob The blob that contains vocabs.
+   * @param merges_blob The blob that contains the merges.
+   * @param added_tokens The added tokens.
+   * @return The created tokenizer.
    */
   static std::unique_ptr<Tokenizer>
   FromBlobByteLevelBPE(const std::string &vocab_blob,
                        const std::string &merges_blob,
                        const std::string &added_tokens = "");
-  /*!
-   * \brief Create SentencePiece.
+  /**
+   * @brief Create SentencePiece.
    *
-   * \param model_blob The blob that contains vocabs.
-   * \return The created tokenizer.
+   * @param model_blob The blob that contains vocabs.
+   * @return The created tokenizer.
    */
   static std::unique_ptr<Tokenizer>
   FromBlobSentencePiece(const std::string &model_blob);
-  /*!
-   * \brief Create RWKVWorldTokenizer.
+  /**
+   * @brief Create RWKVWorldTokenizer.
    *
-   * \param model_blob The blob that contains vocabs.
-   * \return The created tokenizer.
+   * @param model_blob The blob that contains vocabs.
+   * @return The created tokenizer.
    */
   static std::unique_ptr<Tokenizer>
   FromBlobRWKVWorld(const std::string &model_blob);
